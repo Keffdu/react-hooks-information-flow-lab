dc47b5d2769f666d15a13982c64df0f3c3bc8ef7
Kevin Dutzy <kddutz15@gmail.com>
2022-11-25 13:20:01 -0500

    commit tree 97205b11324f7a29ed637bdc4ee559939378997a

---
+++ src/components/Filter.js
@@ -0,0 +1,29 @@
+import Item from "./Item"
+
+
+const itemsToDisplay = items.filter((item) => {
+    if (selectedCategory === "All") return true;
+
+    return item.category === selectedCategory;
+  });
+
+  return (
+    <div className="ShoppingList">
+      <div className="Filter">
+        <select name="filter" onChange={handleCategoryChange}>
+          <option value="All">Filter by category</option>
+          <option value="Produce">Produce</option>
+          <option value="Dairy">Dairy</option>
+          <option value="Dessert">Dessert</option>
+        </select>
+      </div>
+      <ul className="Items">
+        {itemsToDisplay.map((item) => (
+          <Item key={item.id} name={item.name} category={item.category} />
+        ))}
+      </ul>
+    </div>
+  );
+
+
+  export default Filter;
